ff7ad18ef12192dabf1d577b8a30dc62b9e38066
YouennPJ <youenn.pn@gmail.com>
2024-02-02 18:15:37 -0500

    docs: document a remap so I remember it exists

--- lua/youenn/remap.lua
+++ lua/youenn/remap.lua
@@ -23,8 +23,11 @@ vim.keymap.set("n", "<leader>svwm", function()
 end)
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Yank into "+" register (clipboard)
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
