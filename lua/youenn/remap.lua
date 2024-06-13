9c47f8ef22eb4d4bc0e732284f10d47771e4b775
Youenn <youenn.pn@gmail.com>
2024-06-13 09:24:19 -0400

    feat: add remap to align vim horizontally

--- lua/youenn/remap.lua
+++ lua/youenn/remap.lua
@@ -8,6 +8,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "zZ", "zszH")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<C-l>", "<C-w>w")
@@ -40,5 +41,4 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
-
 vim.keymap.set("n", "<Esc>", "<C-\\><C-n>");
