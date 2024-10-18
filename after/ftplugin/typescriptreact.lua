fdd5799b7aae9766c5d55f6a7e059e3758c0bee2
Youenn <youenn.pn@gmail.com>
2024-10-18 14:49:50 -0400

    feat: add remap for use effect and console log

--- after/ftplugin/typescriptreact.lua
+++ after/ftplugin/typescriptreact.lua
@@ -1,3 +1,6 @@
 vim.keymap.set("n", "<leader>cl", 'oconsole.log()<Esc>i', { buffer = true })
 vim.keymap.set("n", "<leader>ef", 'ouseEffect(() => {\
 }, [])<Esc>ko', { buffer = true })
+vim.keymap.set("n", "<leader>ec", 'ouseEffect(() => {\
+console.log()\
+}, [])<Esc>k$i', { buffer = true })
